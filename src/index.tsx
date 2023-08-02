d161215b213c8f6a823c89612aabb8395ac4b7d8
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-08-02 22:46:18 +0300

    Correct basename.

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOMClient.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
